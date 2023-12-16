8203219cb64c153887d23bdc664d3ed604613216
valderyjr <dev.valderyjunior@gmail.com>
2023-12-16 00:05:59 -0300

    fix: create state property in address

--- backend/src/addresses/dto/create-address.dto.ts
+++ backend/src/addresses/dto/create-address.dto.ts
@@ -1,8 +1,18 @@
-import { IsOptional, IsString, IsUUID, Length } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsEnum, IsOptional, IsString, IsUUID, Length } from 'class-validator';
+import { STATES_ENUM } from 'src/enums/states-enum';
 
 export class CreateAddressDto {
   @IsUUID(4)
   userId: string;
+
+  @IsString()
+  @Length(2, 2)
+  @Transform(({ value }) =>
+    typeof value === 'string' ? value.toUpperCase() : value,
+  )
+  @IsEnum(STATES_ENUM)
+  state: string;
 
   @IsString()
   @Length(8, 8)
--- backend/src/addresses/entities/address.entity.ts
+++ backend/src/addresses/entities/address.entity.ts
@@ -2,6 +2,7 @@ export class Address {
   id: string;
   userId: string;
   zipCode: string;
+  state: string;
   city: string;
   street: string;
   number: string;
